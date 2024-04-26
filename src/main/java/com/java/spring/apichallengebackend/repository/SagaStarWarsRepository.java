e37d3a21f455386d98556c70cac216a2a3ad0186
halmeida03 <developerhugoh@gmail.com>
2024-04-26 11:31:58 -0300

    Fix error in custom update in repository

--- src/main/java/com/java/spring/apichallengebackend/repository/SagaStarWarsRepository.java
+++ src/main/java/com/java/spring/apichallengebackend/repository/SagaStarWarsRepository.java
@@ -5,11 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface SagaStarWarsRepository extends JpaRepository<StarWarsEntity, Long> {
 
     @Modifying
-    @Query("update Movie m set m.opening_crawl = :opening_crawl where m.episode_id = :episodeId")
-    void updateDescriptionMovie(@Param(value = "episode_id") Long episodeId, @Param(value = "opening_crawl") String opening_crawl);
+    @Query("update movie m set m.opening_crawl = :opening_crawl where m.episode_id = :episode_id")
+    void updateDescriptionMovie(@Param(value = "episode_id") Long episode_id, @Param(value = "opening_crawl") String opening_crawl);
 }
