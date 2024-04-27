eb4a5fc7273d1fe801c05e71815e3030233f298b
halmeida03 <developerhugoh@gmail.com>
2024-04-27 17:13:25 -0300

    Update api informations

--- src/main/java/com/java/spring/apichallengebackend/resource/swagger/ChallengeResourceSwagger.java
+++ src/main/java/com/java/spring/apichallengebackend/resource/swagger/ChallengeResourceSwagger.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.java.spring.apichallengebackend.domain.Movie;
 import com.java.spring.apichallengebackend.domain.UpdateDescriptionRequest;
+import com.java.spring.apichallengebackend.exception.ErrorResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -31,8 +32,9 @@ public interface ChallengeResourceSwagger {
     description = "Recebe um body com a nova descrição e um id do episódio a ter a descrição alterada")
     @ApiResponses(value = {
                     @ApiResponse(responseCode = "204", description = "Operação realizada com sucesso", content = @Content(schema = @Schema(implementation = Movie.class))),
-                    @ApiResponse(responseCode = "400", description = "Id fornecido é inválido"),
-                    @ApiResponse(responseCode = "404", description = "Episódio inexistente")
+                    @ApiResponse(responseCode = "400", description = "Id fornecido é inválido", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "404", description = "Episódio inexistente, não encontrado", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "500", description = "Ocorreu um erro interno durante operação", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     @PatchMapping(value = "/movie/{episodeId}", consumes = {"application/json"})
     ResponseEntity<Movie> updateMovie(
@@ -44,7 +46,7 @@ public interface ChallengeResourceSwagger {
     description = "Devolve uma lista com informações de todos os filmes da saga Star Wars")
     @ApiResponses(value = {
                     @ApiResponse(responseCode = "200", description = "Operação realizada com sucesso", content = @Content(schema = @Schema(implementation = Movie.class))),
-                    @ApiResponse(responseCode = "500", description = "Ocorreu um erro interno durante operação")
+                    @ApiResponse(responseCode = "500", description = "Ocorreu um erro interno durante operação", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     @GetMapping("/movies")
     ResponseEntity<List<Movie>> listMovies();
@@ -54,7 +56,7 @@ public interface ChallengeResourceSwagger {
     description = "Detalha informações de um filme da saga Star Wars de forma aleatória")
     @ApiResponses(value = {
                     @ApiResponse(responseCode = "200", description = "Operação realizada com sucesso", content = @Content(schema = @Schema(implementation = Movie.class))),
-                    @ApiResponse(responseCode = "500", description = "Ocorreu um erro interno durante operação")
+                    @ApiResponse(responseCode = "500", description = "Ocorreu um erro interno durante operação", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     @GetMapping("/movie/detail")
     ResponseEntity<Movie> getMovie();
@@ -64,9 +66,9 @@ public interface ChallengeResourceSwagger {
     description = "Recebe o id do episódio do filme e detalha informações de um filme específico da saga Star Wars")
     @ApiResponses(value = {
                     @ApiResponse(responseCode = "200", description = "Operação realizada com sucesso", content = @Content(schema = @Schema(implementation = Movie.class))),
-                    @ApiResponse(responseCode = "400", description = "Id fornecido é inválido"),
-                    @ApiResponse(responseCode = "404", description = "Episódio inexistente"),
-                    @ApiResponse(responseCode = "500", description = "Ocorreu um erro interno durante operação")
+                    @ApiResponse(responseCode = "400", description = "Id fornecido é inválido", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "404", description = "Episódio inexistente, não encontrado", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "500", description = "Ocorreu um erro interno durante operação", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     @GetMapping("/movie/detail/{episodeId}")
     ResponseEntity<Movie> getMovie(@Parameter(description = "Id do episódio a ser consultado", required = true) @PathVariable Long episodeId);
